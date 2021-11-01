fd8534fc6b925daf1b4e6c28010fb934110b4e6c
CharlieCallahan <chca7857@colorado.edu>
2021-11-01 16:55:49 -0500

    Updated test file

--- runTest.py
+++ runTest.py
@@ -18,7 +18,16 @@ gs.gaasInit(startWavenum,endWavenum,mol,iso,cwd+"/gaasDir/",'HTData',"test",load
 
 nus_h,coefs_h = gs.runHAPI(T, P, conc, (endWavenum - startWavenum)/wavenumRes, startWavenum, endWavenum, mol, iso,'HTData')
 
-nus, coefs = gs.gaasRunF64(T, P,conc,(endWavenum - startWavenum)/wavenumRes,startWavenum,endWavenum,cwd+"/gaasDir/",mol,iso,"test")
+nus, coefs = gs.gaasRunF32(T, P,conc,(endWavenum - startWavenum)/wavenumRes,startWavenum,endWavenum,cwd+"/gaasDir/",mol,iso,"test")
+
+error = 0
+total = 0 #area 
+
+for i in range(len(coefs)-1):
+    error+=abs(coefs_h[i]-coefs[i])
+    total+=coefs_h[i]
+    
+print("Error: ",error/total*100,"%")
 
 plt.plot(nus,coefs)
 plt.plot(nus_h,coefs_h)
